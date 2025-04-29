953710951744fe8ca32a49c630151b2dea6a733c
Kavee Hewamanna <hewamannakavee@gmail.com>
2025-04-30 01:03:38 +0530

    Create variable for post sharing.

--- learn-grow/src/main/java/com/example/learn/grow/model/Post.java
+++ learn-grow/src/main/java/com/example/learn/grow/model/Post.java
@@ -15,6 +15,9 @@ import lombok.Data;
 //Create model class
 
 public class Post {
+    
+    //Create variables
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,38 +27,5 @@ public class Post {
 
     private String description;
 
-    // Default constructor (required by JPA)
-    public Post() {
-    }
 
-    // Constructor with fields (optional)
-    public Post(String title, String description) {
-        this.title = title;
-        this.description = description;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String decription, String description) {
-                this.description = description;
-    }
 }
