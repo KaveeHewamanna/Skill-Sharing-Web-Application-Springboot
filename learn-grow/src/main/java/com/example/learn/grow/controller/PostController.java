d889d6a15aeafa283e3b945754926ca60d1e9234
Kavee Hewamanna <hewamannakavee@gmail.com>
2025-04-30 01:51:05 +0530

    Make changes to controller by adding file attribute

--- learn-grow/src/main/java/com/example/learn/grow/controller/PostController.java
+++ learn-grow/src/main/java/com/example/learn/grow/controller/PostController.java
@@ -1,21 +1,11 @@
 package com.example.learn.grow.controller;
 
-//Create controller class
 import com.example.learn.grow.model.Post;
-
-import lombok.RequiredArgsConstructor;
 import com.example.learn.grow.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,7 +13,6 @@ import java.util.List;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @CrossOrigin("*")
-
 public class PostController {
 
     private final PostService postService;
@@ -37,23 +26,24 @@ public class PostController {
     public Post getPost(@PathVariable Long id) {
         return postService.getPostById(id);
     }
-    //Add post method to create post
+
+    // Create post with file upload
     @PostMapping("/post")
-    public Post createPost(@RequestBody Post post) {
-        return postService.createPost(post);
+    public Post createPost(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam(value = "file", required = false) MultipartFile file) {
+        return postService.createPost(title, description, file);
     }
-    //Create put method to update post
+
     @PutMapping("/{id}")
     public Post updatePost(@PathVariable Long id, @RequestBody Post postDetails) {
         return postService.updatePost(id, postDetails);
     }
 
-    //Create delete method to delete post by using id
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Long id) {
-       postService.deletePost(id);
+        postService.deletePost(id);
         return ResponseEntity.noContent().build();
     }
-
-    
 }
