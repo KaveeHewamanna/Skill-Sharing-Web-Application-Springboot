078b996fc0965f422264430dcf720ae33ec84cf7
IT22604958 <IT22604958@my.sliit.lk>
2025-04-30 01:24:46 +0530

    create_comment.java

---
+++ learn-grow/src/main/java/com/example/learn/grow/model/Comment.java
@@ -0,0 +1,18 @@
+package com.example.learn.grow.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="comments" )
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+}
