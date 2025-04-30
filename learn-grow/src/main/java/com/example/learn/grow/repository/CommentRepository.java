17d1002eaeac1ed17c66742d0b61e14e6b3a5dfe
IT22604958 <IT22604958@my.sliit.lk>
2025-04-30 06:37:48 +0530

    create_commentRepository

---
+++ learn-grow/src/main/java/com/example/learn/grow/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.learn.grow.repository;
+
+import com.example.learn.grow.model.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    
+    
+}
