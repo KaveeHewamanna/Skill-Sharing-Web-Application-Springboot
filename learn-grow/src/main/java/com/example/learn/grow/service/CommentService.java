bb40a69097f208ce208c9d656ff06c69b3b45cf3
IT22604958 <IT22604958@my.sliit.lk>
2025-04-30 07:09:26 +0530

    getAllComment

---
+++ learn-grow/src/main/java/com/example/learn/grow/service/CommentService.java
@@ -0,0 +1,22 @@
+package com.example.learn.grow.service;
+
+import com.example.learn.grow.model.Comment;
+import com.example.learn.grow.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }
+
+
+    
+}
